ea3a0a80637d2480cca419b8e0e4abadc64a394b
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-01-03 21:59:09 +0100

    Add flag for (unsupported) compilation

--- cmd/go-bf/main.go
+++ cmd/go-bf/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
 	inputPath := flag.String("f", "", "input file for interpreter/compiler")
+	compile := flag.Bool("c", false, "flag for compiling instead of interpreting")
 
 	flag.Parse()
+
+	if *compile {
+		fmt.Println("Compilation currently not supported!")
+		os.Exit(-1)
+	}
 
 	fmt.Printf("%v\n", *inputPath)
 }
