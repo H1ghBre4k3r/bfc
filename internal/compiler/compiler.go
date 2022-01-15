1cb5d8067496959a4b7aec45d16f0693a56f7a4c
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-01-15 23:10:42 +0100

    Compile and link executable during compilation

--- internal/compiler/compiler.go
+++ internal/compiler/compiler.go
@@ -3,7 +3,9 @@ package compiler
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strings"
 
 	"github.com/H1ghBre4k3r/go-bf/internal/input"
 	"github.com/H1ghBre4k3r/go-bf/internal/lexer"
@@ -27,8 +29,12 @@ func New(inputPath string, outputPath string) *Compiler {
 func (c *Compiler) Start() {
 	lexed := lexer.Lex(c.program, c.path)
 	parsed := parser.Parse(lexed, c.path)
+
+	fmt.Printf("[INFO] Generating NASM for %v\n", c.path)
 	compiled := c.compile(parsed)
 	c.saveCode(fmt.Sprintf(base, compiled))
+	c.compileNasm()
+	c.linkNasm()
 }
 
 var label = 0
@@ -73,13 +79,39 @@ func (c *Compiler) compile(parsed []parser.Instruction) string {
 }
 
 func (c *Compiler) saveCode(code string) {
-	outputPath := c.path + ".asm"
-	if c.outputPath != "" {
-		name := filepath.Base(c.path)
-		outputPath = filepath.Join(c.outputPath, name+".asm")
-	}
-	if err := os.WriteFile(outputPath, []byte(code), 0644); err != nil {
+	if err := os.WriteFile(c.outPath(".asm"), []byte(code), 0644); err != nil {
 		fmt.Println(err.Error())
 		os.Exit(-1)
 	}
+}
+
+func (c *Compiler) outPath(ending string) string {
+	outputPath := c.path + ending
+	if c.outputPath != "" {
+		name := filepath.Base(c.path)
+		outputPath = filepath.Join(c.outputPath, name+ending)
+	}
+	return outputPath
+}
+
+func (c *Compiler) compileNasm() {
+	fmt.Printf("[INFO] Compiling NASM for %v\n", c.path)
+	if err := exec.Command("nasm", "-f", "macho64", c.outPath(".asm")).Run(); err != nil {
+		fmt.Printf("[ERROR] could not compile NASM (%v)\n", err.Error())
+	}
+}
+
+func (c *Compiler) linkNasm() {
+	fmt.Printf("[INFO] Linking NASM for %v\n", c.path)
+	if err := exec.Command("ld", "-macos_version_min", "10.12.0", "-L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib", "-lSystem", "-o", c.executablemain(), c.outPath(".o")).Run(); err != nil {
+		fmt.Printf("[ERROR] could not link NASM (%v)\n", err.Error())
+	}
+}
+
+func (c *Compiler) executablemain() string {
+	fileName := c.outPath("")
+	if pos := strings.LastIndexByte(fileName, '.'); pos != -1 {
+		return fileName[:pos]
+	}
+	return fileName
 }
