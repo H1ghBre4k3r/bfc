eae5a99f3802bd22d66d1b1f6e72d4d8207cd330
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-05-28 17:44:20 +0200

    Add basic CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use clap::{IntoApp, Parser};
+
+#[derive(Parser, Debug)]
+#[clap(name = "BFC")]
+#[clap(author, about, version)]
+struct Args {
+    /// input file for interpreter/compiler
+    #[clap(short, long)]
+    file: String,
+
+    /// flag for compiling instead of interpreting
+    #[clap(short, long)]
+    compile: bool,
+
+    /// output directory for compilation files
+    #[clap(short, long)]
+    output: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if args.compile {
+        if let None = args.output.as_deref() {
+            let mut cmd = Args::command();
+            cmd.error(
+                clap::ErrorKind::ArgumentConflict,
+                "output path must be present when trying to compile!",
+            )
+            .exit();
+        }
+    }
+
+    println!("{:?}", args);
 }
