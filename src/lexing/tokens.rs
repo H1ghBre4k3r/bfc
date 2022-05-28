7243542086460983ce1c9e32a9d95410b4e30ac9
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-05-28 18:47:00 +0200

    Add simple unit tests for lexer

--- src/lexing/tokens.rs
+++ src/lexing/tokens.rs
@@ -10,6 +10,7 @@ pub enum Token {
     IN(Position),
 }
 
+/// Get the token associated with a certain brainfuck "character".
 pub fn from(c: char, position: Position) -> Option<Token> {
     match c {
         '<' => Some(Token::LEFT(position)),
@@ -24,8 +25,62 @@ pub fn from(c: char, position: Position) -> Option<Token> {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Position {
     pub line: u64,
     pub column: u64,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{from, Position, Token};
+
+    #[test]
+    fn test_from_correct_tokens() {
+        let dummy_position = Position { line: 0, column: 0 };
+
+        assert!(match from('>', dummy_position.clone()).unwrap() {
+            Token::RIGHT(_) => true,
+            _ => false,
+        });
+        assert!(match from('<', dummy_position.clone()).unwrap() {
+            Token::LEFT(_) => true,
+            _ => false,
+        });
+        assert!(match from('+', dummy_position.clone()).unwrap() {
+            Token::PLUS(_) => true,
+            _ => false,
+        });
+        assert!(match from('-', dummy_position.clone()).unwrap() {
+            Token::MINUS(_) => true,
+            _ => false,
+        });
+        assert!(match from('[', dummy_position.clone()).unwrap() {
+            Token::START_LOOP(_) => true,
+            _ => false,
+        });
+        assert!(match from(']', dummy_position.clone()).unwrap() {
+            Token::END_LOOP(_) => true,
+            _ => false,
+        });
+        assert!(match from('.', dummy_position.clone()).unwrap() {
+            Token::OUT(_) => true,
+            _ => false,
+        });
+        assert!(match from(',', dummy_position.clone()).unwrap() {
+            Token::IN(_) => true,
+            _ => false,
+        });
+    }
+
+    #[test]
+    fn test_from_non_tokens() {
+        let dummy_position = Position { line: 0, column: 0 };
+
+        assert!(from('a', dummy_position.clone()).is_none());
+        assert!(from('x', dummy_position.clone()).is_none());
+        assert!(from(' ', dummy_position.clone()).is_none());
+        assert!(from('?', dummy_position.clone()).is_none());
+        assert!(from('ä', dummy_position.clone()).is_none());
+    }
 }
